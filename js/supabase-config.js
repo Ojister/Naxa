ba8aa9a0d25975e68f19e48096e893adff66dee9
Ojister <abubakarusmanosagede65@gmail.com>
2026-08-23 20:24:35 +0100

    Create js/supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://srjnejklbyewbirmiskw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_MY_yf9UCO5GsvdWO855LCg_BWDbu4RL";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
